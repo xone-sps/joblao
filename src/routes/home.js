dbfed0cb47df0c33e2c6ef51ac575d6895d410c0
xone-sps <xone.sps@gmail.com>
2019-12-19 22:49:47 +0700

    first commit

---
+++ src/routes/home.js
@@ -0,0 +1,8 @@
+import Home from '@/components/General/Home.vue'
+export default [
+  {
+    path:'/',
+    components: Home,
+    name:'home'
+  }
+]
---
+++ src/starter/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+  <Header/>
+    <router-view></router-view>
+  <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Partial/Header'
+import Footer from '@/components/Partial/Footer'
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+body{
+  padding: 0;
+  margin:0;
+}
+</style>
